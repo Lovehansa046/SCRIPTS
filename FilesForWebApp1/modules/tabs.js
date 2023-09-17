c8f5dc2df9364c687c3916084198920cea35ceff
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2023-09-17 18:22:40 +0300

    SCRIP-8 Write 50% function 'tabs'

---
+++ FilesForWebApp1/modules/tabs.js
@@ -0,0 +1,26 @@
+// Writing Code in the Function('tabs')
+// I will write up the code later(posible monday(18-09 or 20-09-2023))
+
+let tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    });
+}
+
+function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+hideTabContent();
+showTabContent();
